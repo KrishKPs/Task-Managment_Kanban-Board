9b10a7f53cd7e7009acca14acc9620dbd39fbaf4
KrishKPs <kpskrishpatel@gmail.com>
2024-09-25 18:10:44 -0700

    Added CORS Origin

--- src/Middleware/cors.js
+++ src/Middleware/cors.js
@@ -3,7 +3,7 @@ import Cors from 'cors';
 
 // CORS middleware to allow requests from specific domains
 const cors = Cors({
-  origin: ['https://task-managment-kanban-mmhesu1y6-krish-patels-projects-3e6b9326.vercel.app'],  // Add other origins as needed
+  origin: '*',  // Add other origins as needed
   methods: ['GET', 'POST', 'PUT', 'DELETE' , 'OPTIONS'],
   credentials : true,     // Allow 'Access-Control-Allow-Credentials'  
   allowedHeaders : ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With']
